bd50e7b97617c1202832879d6ea819f33c75bbef
Joost Buiting <joost.j.buiting@student.hu.nl>
2020-03-30 11:53:01 +0200

    Initial commit

---
+++ src/project/Docent.java
@@ -0,0 +1,6 @@
+package project;
+
+public class Docent extends Persoon {
+
+
+}
---
+++ src/project/Klas.java
@@ -0,0 +1,12 @@
+package project;
+
+
+public class Klas {
+    private String klasCode;
+    private int aantalLeerlingen;
+
+    public Klas(String kC, int aL){
+        klasCode = kC;
+        aantalLeerlingen = aL;
+    }
+}
